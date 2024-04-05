0de2740bbd7062c0dc914ced7116feeb2684bb62
anass zeroual <anasszeroual09@gmail.com>
2024-04-05 13:53:02 +0000

    cp

---
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/ActionPlanServiceImpl.java
@@ -0,0 +1,11 @@
+package com.dpm.dailyPerformanceManagement.services.impl;
+
+import com.dpm.dailyPerformanceManagement.services.ActionPlanService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class ActionPlanServiceImpl implements ActionPlanService {
+
+}
