0d892cd4008cfaa04eb8cc29ef4339f1f9d7ac6a
anass zeroual <anasszeroual09@gmail.com>
2024-04-05 15:25:02 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/controller/InventoryController.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/InventoryController.java
@@ -1,9 +1,14 @@
 package com.dpm.dailyPerformanceManagement.controller;
 
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
 import com.dpm.dailyPerformanceManagement.models.RequestModel;
 import com.dpm.dailyPerformanceManagement.services.InventoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -21,6 +26,13 @@ public class InventoryController {
         }
     }
 
-
+    @PostMapping("/actionPlan")
+    public void addActionPlan(@RequestBody List<ActionPlanModel> actionPlanModel, @RequestParam String name, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        try {
+            inventoryService.addActionPlan(actionPlanModel, name, date);
+        } catch (Error error) {
+            throw new RuntimeException(error);
+        }
+    }
 
 }
--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/InventoryServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/InventoryServiceImpl.java
@@ -1,7 +1,9 @@
 package com.dpm.dailyPerformanceManagement.services.impl;
 
+import com.dpm.dailyPerformanceManagement.domain.ActionPlan;
 import com.dpm.dailyPerformanceManagement.domain.DataByDate;
 import com.dpm.dailyPerformanceManagement.domain.Inventory;
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
 import com.dpm.dailyPerformanceManagement.models.RequestModel;
 import com.dpm.dailyPerformanceManagement.repositories.ActionPlanRepo;
 import com.dpm.dailyPerformanceManagement.repositories.DataByDateRepo;
@@ -10,6 +12,9 @@ import com.dpm.dailyPerformanceManagement.services.InventoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -60,5 +65,33 @@ public class InventoryServiceImpl implements InventoryService {
         d.setRealValue(rm.getReal());
         d.setName(rm.getName());
         return d;
+    }
+
+
+    @Override
+    public void addActionPlan(List<ActionPlanModel> apm, String name, Date date) {
+        DataByDate dbd = dataByDateRepo.findByDateDpm(date);
+        if (!dbd.getInventories().isEmpty()) {
+            Optional<Inventory> deliveryWithNameAp =
+                    dbd.getInventories().stream().filter(delivery -> delivery.getName().equals(name)).findFirst();
+
+            if (deliveryWithNameAp.isPresent()) {
+                Inventory delivery = deliveryWithNameAp.get();
+                List<ActionPlan> aps = new ArrayList<>();
+                for (ActionPlanModel ap : apm) {
+                    ActionPlan acp = new ActionPlan();
+                    acp.setResp(ap.getResp());
+                    acp.setCauses(ap.getCauses());
+                    acp.setContermeasures(ap.getContermeasures());
+                    acp.setResp(ap.getResp());
+                    acp.setDueDate(ap.getDueDate());
+                    acp.setStatus(ap.getStatus());
+                    acp.setIssueDescription(ap.getIssueDescription());
+                    acp.setInventory(delivery);
+                    aps.add(acp);
+                }
+                actionPlanRepo.saveAll(aps);
+            }
+        }
     }
 }
