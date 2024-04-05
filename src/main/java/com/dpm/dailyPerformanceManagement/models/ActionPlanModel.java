3c843fa6d484223954b8ae1244d11e4a58f2e2fd
anass zeroual <anasszeroual09@gmail.com>
2024-04-05 14:09:50 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/models/ActionPlanModel.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/models/ActionPlanModel.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.util.Date;
 @Data
 public class ActionPlanModel {
+    private Long id;
     private String issueDescription;
     private String causes;
     private String contermeasures;
--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/DeliveryServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/DeliveryServiceImpl.java
@@ -1,15 +1,20 @@
 package com.dpm.dailyPerformanceManagement.services.impl;
 
+import com.dpm.dailyPerformanceManagement.domain.ActionPlan;
 import com.dpm.dailyPerformanceManagement.domain.DataByDate;
 import com.dpm.dailyPerformanceManagement.domain.Delivery;
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
 import com.dpm.dailyPerformanceManagement.models.RequestModel;
+import com.dpm.dailyPerformanceManagement.repositories.ActionPlanRepo;
 import com.dpm.dailyPerformanceManagement.repositories.DataByDateRepo;
 import com.dpm.dailyPerformanceManagement.repositories.DeliveryRepo;
 import com.dpm.dailyPerformanceManagement.services.DeliveryService;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,21 +23,21 @@ import java.util.Optional;
 public class DeliveryServiceImpl implements DeliveryService {
     DeliveryRepo deliveryRepo;
     DataByDateRepo dataByDateRepo;
+    ActionPlanRepo actionPlanRepo;
+
     @Override
-    public void addDeliveryData(RequestModel rm){
-        DataByDate dbd= dataByDateRepo.findByDateDpm(rm.getDate());
-        if (dbd==null){
-            Delivery d= extractedDelivery(rm);
-            DataByDate dataByDate=new DataByDate();
+    public void addDeliveryData(RequestModel rm) {
+        DataByDate dbd = dataByDateRepo.findByDateDpm(rm.getDate());
+        if (dbd == null) {
+            Delivery d = extractedDelivery(rm);
+            DataByDate dataByDate = new DataByDate();
             dataByDate.setDateDpm(rm.getDate());
-            dataByDate= dataByDateRepo.save(dataByDate);
+            dataByDate = dataByDateRepo.save(dataByDate);
             d.setDbd(dataByDate);
             deliveryRepo.save(d);
-        }else {
-            if (!dbd.getDeliveries().isEmpty()){
-                Optional<Delivery> deliveryWithNameAp = dbd.getDeliveries().stream()
-                        .filter(delivery -> delivery.getName().equals(rm.getName()))
-                        .findFirst();
+        } else {
+            if (!dbd.getDeliveries().isEmpty()) {
+                Optional<Delivery> deliveryWithNameAp = dbd.getDeliveries().stream().filter(delivery -> delivery.getName().equals(rm.getName())).findFirst();
 
                 if (deliveryWithNameAp.isPresent()) {
                     Delivery delivery = deliveryWithNameAp.get();
@@ -40,7 +45,7 @@ public class DeliveryServiceImpl implements DeliveryService {
                     delivery.setTargetValue(rm.getTarget());
                     deliveryRepo.save(delivery);
                 } else {
-                    Delivery d= extractedDelivery(rm);
+                    Delivery d = extractedDelivery(rm);
                     d.setDbd(dbd);
                     deliveryRepo.save(d);
                 }
@@ -49,11 +54,35 @@ public class DeliveryServiceImpl implements DeliveryService {
     }
 
     private Delivery extractedDelivery(RequestModel rm) {
-        Delivery d=new Delivery();
+        Delivery d = new Delivery();
         d.setTargetValue(rm.getTarget());
         d.setRealValue(rm.getReal());
         d.setName(rm.getName());
         return d;
     }
 
+
+    public void addActionPlan(List<ActionPlanModel> apm, String name, Date date) {
+        DataByDate dbd = dataByDateRepo.findByDateDpm(date);
+        if (!dbd.getDeliveries().isEmpty()) {
+            Optional<Delivery> deliveryWithNameAp = dbd.getDeliveries().stream().filter(delivery -> delivery.getName().equals(name)).findFirst();
+
+            if (deliveryWithNameAp.isPresent()) {
+                Delivery delivery = deliveryWithNameAp.get();
+                List<ActionPlan> aps = new ArrayList<>();
+                for (ActionPlanModel ap : apm) {
+                    ActionPlan acp = new ActionPlan();
+                    acp.setResp(ap.getResp());
+                    acp.setCauses(ap.getCauses());
+                    acp.setContermeasures(ap.getContermeasures());
+                    acp.setResp(ap.getResp());
+                    acp.setDueDate(ap.getDueDate());
+                    acp.setStatus(ap.getStatus());
+                    acp.setDelivery(delivery);
+                    aps.add(acp);
+                }
+                actionPlanRepo.saveAll(aps);
+            }
+        }
+    }
 }
