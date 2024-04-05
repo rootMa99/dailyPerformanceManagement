9a9aaf1613dc411b1e6ece2c4ed21719229b54d3
rootMa99 <anasszeroual09@gmail.com>
2024-04-05 20:22:33 +0000

    cp

---
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/SkillsController.java
@@ -0,0 +1,39 @@
+package com.dpm.dailyPerformanceManagement.controller;
+
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
+import com.dpm.dailyPerformanceManagement.models.RequestModel;
+import com.dpm.dailyPerformanceManagement.services.SkillsService;
+import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/skills")
+@AllArgsConstructor
+public class SkillsController {
+    SkillsService skillsService;
+
+    @PostMapping
+    public void addData(@RequestBody RequestModel rm) {
+        try {
+            skillsService.addSkillsData(rm);
+        } catch (Error error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+    @PostMapping("/actionPlan")
+    public void addActionPlan(@RequestBody List<ActionPlanModel> actionPlanModel, @RequestParam String name, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        try {
+            skillsService.addActionPlan(actionPlanModel, name, date);
+        } catch (Error error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+
+}
