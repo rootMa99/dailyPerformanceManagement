9b3676d74c3d3cfaacc769d5a6154f243e235401
anass zeroual <anasszeroual09@gmail.com>
2024-04-05 15:50:13 +0000

    cp

---
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/Productivitycontroller.java
@@ -0,0 +1,38 @@
+package com.dpm.dailyPerformanceManagement.controller;
+
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
+import com.dpm.dailyPerformanceManagement.models.RequestModel;
+import com.dpm.dailyPerformanceManagement.services.ProductivityService;
+import lombok.AllArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/productivity")
+@AllArgsConstructor
+public class Productivitycontroller {
+
+    ProductivityService productivityService;
+
+    @PostMapping
+    public void addData(@RequestBody RequestModel rm) {
+        try {
+            productivityService.addProductivityData(rm);
+        } catch (Error error) {
+            throw new RuntimeException(error);
+        }
+    }
+
+    @PostMapping("/actionPlan")
+    public void addActionPlan(@RequestBody List<ActionPlanModel> actionPlanModel, @RequestParam String name, @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+        try {
+            productivityService.addActionPlan(actionPlanModel, name, date);
+        } catch (Error error) {
+            throw new RuntimeException(error);
+        }
+    }
+}
--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/ProductivityServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/ProductivityServiceImpl.java
@@ -1,10 +1,95 @@
 package com.dpm.dailyPerformanceManagement.services.impl;
 
+import com.dpm.dailyPerformanceManagement.domain.ActionPlan;
+import com.dpm.dailyPerformanceManagement.domain.DataByDate;
+import com.dpm.dailyPerformanceManagement.domain.Productivity;
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
+import com.dpm.dailyPerformanceManagement.models.RequestModel;
+import com.dpm.dailyPerformanceManagement.repositories.ActionPlanRepo;
+import com.dpm.dailyPerformanceManagement.repositories.DataByDateRepo;
+import com.dpm.dailyPerformanceManagement.repositories.ProductivityRepo;
 import com.dpm.dailyPerformanceManagement.services.ProductivityService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class ProductivityServiceImpl implements ProductivityService {
+    ProductivityRepo productivityRepo;
+    DataByDateRepo dataByDateRepo;
+    ActionPlanRepo actionPlanRepo;
+
+    @Override
+    public void addProductivityData(RequestModel rm) {
+        DataByDate dbd = dataByDateRepo.findByDateDpm(rm.getDate());
+        if (dbd == null) {
+            Productivity d = extractedDelivery(rm);
+            DataByDate dataByDate = new DataByDate();
+            dataByDate.setDateDpm(rm.getDate());
+            dataByDate = dataByDateRepo.save(dataByDate);
+            d.setDbd(dataByDate);
+            productivityRepo.save(d);
+        } else {
+            if (!dbd.getProductivities().isEmpty()) {
+                Optional<Productivity> deliveryWithNameAp =
+                        dbd.getProductivities().stream().filter(delivery -> delivery.getName().equals(rm.getName())).findFirst();
+
+                if (deliveryWithNameAp.isPresent()) {
+                    Productivity delivery = deliveryWithNameAp.get();
+                    delivery.setRealValue(rm.getReal());
+                    delivery.setTargetValue(rm.getTarget());
+                    productivityRepo.save(delivery);
+                } else {
+                    Productivity d = extractedDelivery(rm);
+                    d.setDbd(dbd);
+                    productivityRepo.save(d);
+                }
+            } else {
+                Productivity d = extractedDelivery(rm);
+                d.setDbd(dbd);
+                productivityRepo.save(d);
+            }
+        }
+    }
+
+    private Productivity extractedDelivery(RequestModel rm) {
+        Productivity d = new Productivity();
+        d.setTargetValue(rm.getTarget());
+        d.setRealValue(rm.getReal());
+        d.setName(rm.getName());
+        return d;
+    }
+
+    @Override
+    public void addActionPlan(List<ActionPlanModel> apm, String name, Date date) {
+        DataByDate dbd = dataByDateRepo.findByDateDpm(date);
+        if (!dbd.getProductivities().isEmpty()) {
+            Optional<Productivity> deliveryWithNameAp =
+                    dbd.getProductivities().stream().filter(delivery -> delivery.getName().equals(name)).findFirst();
+
+            if (deliveryWithNameAp.isPresent()) {
+                Productivity delivery = deliveryWithNameAp.get();
+                List<ActionPlan> aps = new ArrayList<>();
+                for (ActionPlanModel ap : apm) {
+                    ActionPlan acp = new ActionPlan();
+                    acp.setResp(ap.getResp());
+                    acp.setCauses(ap.getCauses());
+                    acp.setContermeasures(ap.getContermeasures());
+                    acp.setResp(ap.getResp());
+                    acp.setDueDate(ap.getDueDate());
+                    acp.setStatus(ap.getStatus());
+                    acp.setIssueDescription(ap.getIssueDescription());
+                    acp.setProductivity(delivery);
+                    aps.add(acp);
+                }
+                actionPlanRepo.saveAll(aps);
+            }
+        }
+    }
+
 }
