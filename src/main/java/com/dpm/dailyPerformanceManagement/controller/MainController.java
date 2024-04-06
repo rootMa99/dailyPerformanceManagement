a4cab45144f507e1b9cf0e6c74c0e5c00b90c390
rootMa99 <anasszeroual09@gmail.com>
2024-04-06 20:55:31 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
@@ -26,46 +26,54 @@ public class MainController {
 
     @GetMapping(path = "/delivery")
     public List<DataRest> getDelivery(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                      @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getDelivriesDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/inventory")
     public List<DataRest> getInventory(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getInventoryDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/kaizen")
     public List<DataRest> getKaizen(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                       @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getKaizenDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/productivity")
     public List<DataRest> getProductivity(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                          @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getProductivityDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/quality")
     public List<DataRest> getQuality(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                          @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                     @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getQualityDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/safety")
     public List<DataRest> getSafety(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                     @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getSafetyDateBetween(startDate, endDate);
     }
+
     @GetMapping(path = "/skills")
     public List<DataRest> getSkkills(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
-                                    @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+                                     @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getSkillsDateBetween(startDate, endDate);
     }
+
     @GetMapping("/downloadFile/{fileId:.+}")
     public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileId, HttpServletRequest request) throws FileNotFoundException {
         Files fileEntity = dataByDateService.getFileByFileId(fileId);
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileEntity.getFileType())).header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileEntity.getFileName() + "\"").body(new ByteArrayResource(fileEntity.getData()));
     }
+
     @GetMapping("/owners")
-    public List<KpiRest> getAllOwners(){
+    public List<KpiRest> getAllOwners() {
         return dataByDateService.getAllKpiOwner();
     }
 
