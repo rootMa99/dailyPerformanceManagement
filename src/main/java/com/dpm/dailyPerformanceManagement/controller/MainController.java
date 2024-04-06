df93b65219040ae9fb662115c17ac650f5534c3f
rootMa99 <anasszeroual09@gmail.com>
2024-04-06 20:36:17 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
@@ -1,11 +1,19 @@
 package com.dpm.dailyPerformanceManagement.controller;
 
+import com.dpm.dailyPerformanceManagement.domain.Files;
 import com.dpm.dailyPerformanceManagement.models.DataRest;
+import com.dpm.dailyPerformanceManagement.models.KpiRest;
 import com.dpm.dailyPerformanceManagement.services.DataByDateService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
 import java.util.Date;
 import java.util.List;
 
@@ -51,4 +59,14 @@ public class MainController {
                                     @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getSkillsDateBetween(startDate, endDate);
     }
+    @GetMapping("/downloadFile/{fileId:.+}")
+    public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable String fileId, HttpServletRequest request) throws FileNotFoundException {
+        Files fileEntity = dataByDateService.getFileByFileId(fileId);
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(fileEntity.getFileType())).header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileEntity.getFileName() + "\"").body(new ByteArrayResource(fileEntity.getData()));
+    }
+    @GetMapping("/owners")
+    public List<KpiRest> getAllOwners(){
+        return dataByDateService.getAllKpiOwner();
+    }
+
 }
