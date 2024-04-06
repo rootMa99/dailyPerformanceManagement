dbe18f7518d663fc0472ed5439d7689fa3386379
anass zeroual <anasszeroual09@gmail.com>
2024-04-06 10:35:25 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/DataByDateServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/DataByDateServiceImpl.java
@@ -1,13 +1,16 @@
 package com.dpm.dailyPerformanceManagement.services.impl;
 
 
+import com.dpm.dailyPerformanceManagement.domain.ActionPlan;
 import com.dpm.dailyPerformanceManagement.domain.Delivery;
+import com.dpm.dailyPerformanceManagement.models.ActionPlanModel;
 import com.dpm.dailyPerformanceManagement.models.DataRest;
 import com.dpm.dailyPerformanceManagement.repositories.DeliveryRepo;
 import com.dpm.dailyPerformanceManagement.services.DataByDateService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -22,8 +25,38 @@ public class DataByDateServiceImpl implements DataByDateService {
     public List<DataRest> getDelivriesDateBetween(Date start, Date end){
 
         List<Delivery> ds = deliveryRepo.findAllByDbdDateDpmBetween(start, end);
-        System.out.println(ds);
-        return null;
+
+        List<DataRest> drs= new ArrayList<>();
+
+        for (Delivery d:ds){
+            DataRest dr=new DataRest();
+            dr.setDDate(d.getDbd().getDateDpm());
+            dr.setReal(d.getRealValue());
+            dr.setTarget(d.getTargetValue());
+            dr.setName(d.getName());
+            List<ActionPlanModel> apms=new ArrayList<>();
+            for (ActionPlan ap: d.getActionPlans()){
+                ActionPlanModel apm=new ActionPlanModel();
+                apm.setId(ap.getId());
+                apm.setIssueDescription(ap.getIssueDescription());
+                apm.setCauses(ap.getCauses());
+                apm.setContermeasures(ap.getContermeasures());
+                apm.setResp(ap.getResp());
+                apm.setDueDate(ap.getDueDate());
+                apm.setStatus(ap.getStatus());
+                apms.add(apm);
+            }
+            dr.setApm(apms);
+            drs.add(dr);
+        }
+
+
+
+        return drs;
     }
+
+
+
+
 
 }
