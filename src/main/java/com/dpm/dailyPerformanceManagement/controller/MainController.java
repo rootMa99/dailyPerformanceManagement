97791dadaa963104396fdb7a0c67e76cb3bfb9f5
rootMa99 <anasszeroual09@gmail.com>
2024-04-06 19:49:41 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/controller/MainController.java
@@ -16,10 +16,15 @@ import java.util.List;
 public class MainController {
     DataByDateService dataByDateService;
 
-    @GetMapping(path = "/deliveryDataBetween")
-    public List<DataRest> getDataBetween(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
+    @GetMapping(path = "/delivery")
+    public List<DataRest> getDelivery(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
                                          @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
         return dataByDateService.getDelivriesDateBetween(startDate, endDate);
+    }
+    @GetMapping(path = "/inventory")
+    public List<DataRest> getInventory(@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date endDate,
+                                         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date startDate) {
+        return dataByDateService.getInventoryDateBetween(startDate, endDate);
     }
 
 
