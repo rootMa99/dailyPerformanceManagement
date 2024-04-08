2c8c3169737b3936e3b638a0949f705ed13a5046
anass zeroual <anasszeroual09@gmail.com>
2024-04-08 08:18:57 +0000

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/KpiNamesServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/KpiNamesServiceImpl.java
@@ -1,6 +1,6 @@
 package com.dpm.dailyPerformanceManagement.services.impl;
 
-import com.dpm.dailyPerformanceManagement.domain.DKpiNames;
+import com.dpm.dailyPerformanceManagement.domain.*;
 import com.dpm.dailyPerformanceManagement.repositories.*;
 import com.dpm.dailyPerformanceManagement.services.KpiNamesService;
 import lombok.AllArgsConstructor;
@@ -24,11 +24,45 @@ public class KpiNamesServiceImpl implements KpiNamesService {
         List<String> ls = new ArrayList<>();
         if (name.equals("delivery")) {
             List<DKpiNames> d = dKpiNamesRepo.findAll();
-
             for (DKpiNames dk : d) {
                 ls.add(dk.getKpiName());
             }
-
+        }
+        if (name.equals("safety")) {
+            List<SfKpiNames> d = sfKpiNamesRepo.findAll();
+            for (SfKpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
+        }
+        if (name.equals("skills")) {
+            List<SkKpiNames> d = skKpiNamesRepo.findAll();
+            for (SkKpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
+        }
+        if (name.equals("quality")) {
+            List<QKpiNames> d = qKpiNamesRepo.findAll();
+            for (QKpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
+        }
+        if (name.equals("kaizen")) {
+            List<KKpiNames> d = kKpiNamesRepo.findAll();
+            for (KKpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
+        }
+        if (name.equals("inventory")) {
+            List<IKpiNames> d = iKpiNamesRepo.findAll();
+            for (IKpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
+        }
+        if (name.equals("productivity")) {
+            List<PkpiNames> d = pKpiNamesRepo.findAll();
+            for (PkpiNames dk : d) {
+                ls.add(dk.getKpiName());
+            }
         }
         return ls;
     }
