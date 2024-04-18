f33f398bca16b34e4bd8bbb46d7e8c60b9d0daec
anass zeroual <anasszeroual09@gmail.com>
2024-04-18 11:09:24 +0100

    cp

--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/ProductivityServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/ProductivityServiceImpl.java
@@ -115,11 +115,19 @@ public class ProductivityServiceImpl implements ProductivityService {
                     if (pm.getMotif().isEmpty()){
                         continue;
                     }
-                    Pareto p=new Pareto();
-                    p.setMotif(pm.getMotif());
-                    p.setPercentage(pm.getPercentage());
-                    p.setProductivity(delivery);
-                    pmsPrime.add(p);
+                    Pareto fp = paretoRepo.findByMotif(pm.getMotif());
+                    if (fp == null) {
+                        Pareto p = new Pareto();
+                        p.setMotif(pm.getMotif());
+                        p.setPercentage(pm.getPercentage());
+                        p.setProductivity(delivery);
+                        pmsPrime.add(p);
+                    } else {
+                        fp.setMotif(pm.getMotif());
+                        fp.setPercentage(pm.getPercentage());
+                        fp.setProductivity(delivery);
+                        pmsPrime.add(fp);
+                    }
                 }
                 paretoRepo.saveAll(pmsPrime);
             }
--- src/main/java/com/dpm/dailyPerformanceManagement/services/impl/QualityServiceImpl.java
+++ src/main/java/com/dpm/dailyPerformanceManagement/services/impl/QualityServiceImpl.java
@@ -117,11 +117,19 @@ public class QualityServiceImpl implements QualityService {
                     if (pm.getMotif().isEmpty()){
                         continue;
                     }
-                    Pareto p=new Pareto();
-                    p.setMotif(pm.getMotif());
-                    p.setPercentage(pm.getPercentage());
-                    p.setQuality(delivery);
-                    pmsPrime.add(p);
+                    Pareto fp = paretoRepo.findByMotif(pm.getMotif());
+                    if (fp == null) {
+                        Pareto p = new Pareto();
+                        p.setMotif(pm.getMotif());
+                        p.setPercentage(pm.getPercentage());
+                        p.setQuality(delivery);
+                        pmsPrime.add(p);
+                    } else {
+                        fp.setMotif(pm.getMotif());
+                        fp.setPercentage(pm.getPercentage());
+                        fp.setQuality(delivery);
+                        pmsPrime.add(fp);
+                    }
                 }
                 paretoRepo.saveAll(pmsPrime);
             }
